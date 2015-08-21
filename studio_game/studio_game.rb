1b160b9798b9e60a969f603e9e653c0cc65acea3
kangkyu <kangkyu1111@gmail.com>
2015-08-21 14:38:16 -0700

    more exercises for numbers-strings
    add into studio_game with player name and player health value variable,
    let it print them out as a list of names, and string presentation.
    
    larry's health is 60
    
    Players:
      larry
      curly
      moe

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,21 +1,55 @@
-# We want the game to start by printing a welcome message. So in your studio_game.rb file, type in the Ruby code to print "Welcome!" to the screen. 
+# The goal of this exercise is to print out info about our initial player and a list of all the players.
 
-# Remember that irb always prints results to the screen, whereas in a Ruby program file you need to use puts to print results to the screen.
+# Each player has a name (a string) and an initial health value (an integer number).
 
-puts 'Welcome!'
+=begin
+larry's health is 60
+
+Players: 
+  larry
+  curly
+  moe
+=end
+
+
+# The name of our first player is "larry" (he prefers the lowercase pronunciation). Create a variable to remember that name. In other words, assign the player's name to the variable.
+
+# Larry's initial health value is 60. Create a second variable and assign the health value to it.
+
+name = "larry"
+health_value = 60
+
+# Next, using those two variables, create the single-quoted string "larry's health is 60" and print it out to the console
+
+puts name + '\'s health is ' + health_value.to_s
+
+# All that escaping and concatenation gets kinda tedious, doesn't it? Let's change the single-quoted string to a double-quoted string and substitute (interpolate) the name and health values into the string.
 
 puts
-# Now, similar to what we did in the video, change your program to assign the welcome message to a variable named greeting, and print it uppercase three times in a loop.
+puts "#{name}'s health is #{health_value}"
 
-greeting = 'Welcome!'
-
-3.times do
-  puts greeting.upcase
-end
-
-# strings, variables, methods, and blocks
+# How would you change the double-quoted string to triple Larry's health?
 
 puts
-# After printing the greeting, it might be handy for the game to print the time it started. Use the built-in Time class to print the current time.
+puts "#{name}'s health is #{health_value * 3}"
 
-puts Time.new
+puts
+puts "#{name}'s health is #{health_value / 9}"
+puts "#{name}'s health is #{health_value / 9.0}"
+
+# try 1
+puts \
+"Players: 
+  larry
+  curly
+  moe"
+
+# try 2
+puts "Players:\n\tlarry\n\tcurly\n\tmoe"
+
+# try 3
+name1 = 'larry'
+name2 = 'curly'
+name3 = 'moe'
+
+puts "Players:\n\t#{name1}\n\t#{name2}\n\t#{name3}"
