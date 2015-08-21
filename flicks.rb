4ea236d4df4fdb5ed5a183fba948defb087af7d6
kangkyu <kangkyu1111@gmail.com>
2015-08-21 13:41:29 -0700

    numbers and strings
    use double quote when interpolation of expression, or
    support other escape sequences single quote strings don't \t \n etc

---
+++ flicks.rb
@@ -0,0 +1,28 @@
+# puts 'Mikey loves Goonies'
+# puts 'Mikey\'s favorite movie is Goonies'
+
+# puts
+
+# movie = 'Goonies'
+# puts 'Mikey\'s favorite movie is ' + movie
+
+# puts
+
+# rank = 10
+# puts movie + ' has a rank of ' + rank.to_s
+
+puts "Mikey loves Goonies"
+puts "Mikey's favorite movie is Goonies"
+
+puts
+
+movie = 'Goonies'
+puts "Mikey's favorite movie is #{movie}"
+
+puts
+
+rank = 10
+puts "#{movie} has a rank of #{rank}"
+
+# use double quote when interpolation of expression, or
+# support other escape sequences single quote strings don't \t \n etc
