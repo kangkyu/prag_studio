a90a4bda02b12fb8701d4cd495fa0424fde396e4
kangkyu <kangkyu1111@gmail.com>
2015-08-21 16:17:59 -0700

    summary of studio_game/studio_game so far
    it has all different format for the same feature
    it duplicates code

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -1,51 +1,17 @@
-# In this exercise, we'll give them some life and also add a fourth player. The goal is to print out each player's name and health with a slightly different format, like so:
-
-=begin
-Larry has a health of 60.
-CURLY has a health of 125.
-*************Moe has a health of 100.*************
-Shemp........................... 90 health
-=end
-
-# Variables and Objects
-
-name1 = "larry"
+name1 = 'larry'
 health1 = 60
-
-# formatting task : capitalize the name.
-
-name1.capitalize
 puts "#{name1.capitalize} has a health of #{health1}."
 
-name2 = "curly"
+name2 = 'curly'
 health2 = 125
-
 puts "#{name2.upcase} has a health of #{health2}."
 
-# change Curly's health variable to point to Larry's health variable and print out Curly's information again.
-
-health2 = health1
-puts "#{name2.upcase} has a health of #{health2}."
-
-# Reassign Larry's health variable to be 30. Then print out the name and health of both players again.
-
-health1 = 30
-puts "#{name1.capitalize} has a health of #{health1}."
-puts "#{name2.upcase} has a health of #{health2}."
-
-# Use the documentation to find the method that centers a string. Note that the first parameter (the length) is required, but the second parameter for the character to use as padding is optional.
-
-name3 = "moe"
+name3 = 'moe'
 health3 = 100
+text = "#{name3.capitalize} has a health of #{health3}."
+puts text.center(50, '*')
 
-puts "#{name3.capitalize} has a health of #{health3}.".center(50, '#')
-
-name4 = "shemp"
+name4 = 'shemp'
 health4 = 90
-
-puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
-
-# all objects in Ruby have a to_s method that converts the object to its string representation. Using this tip, try chaining methods together to reverse the number 123 again.
-
-puts r = 123.to_s.reverse
-puts r.to_i
+formatted_name = name4.capitalize.ljust(30, '.')
+puts "#{formatted_name} #{health4} health"
