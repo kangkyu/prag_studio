aac04f0985c571e39ded8c9d660fa974238e6a79
kangkyu <kangkyu1111@gmail.com>
2015-08-27 10:40:15 -0700

    To learn more about using objects together, refer to Section 3.2, Classes Working with Other Classes in Programming Ruby.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -30,44 +30,44 @@ class Player
   end
 end
 
+class Game
+  def initialize(title)
+    @title = title.capitalize
+    @players = []
+  end
+
+  def add_player(player)
+    @players << player
+  end
+
+  def play
+    puts "There are #{@players.count} players in #{@title}:"
+    @players.each do |player|
+      puts player
+    end
+  end
+end
+
 # We currently have three Player objects that we created this way:
 
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
 
-# Then, once we have our player objects in an array, we can iterate through them and do the same thing to each player. We'll start by iterating through them and printing out each player's information like this:
+# The objective is to be able to run this code:
+
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
+
+# and get this output:
 =begin
 
-There are 3 players in the game:
+There are 3 players in Knuckleheads: 
 I'm Moe with a health of 100 and a score of 103.
 I'm Larry with a health of 60 and a score of 65.
 I'm Curly with a health of 125 and a score of 130.
-
-=end
-
-players = [player1, player2, player3]
-players.each do |player|
-  player.blam
-  player.w00t
-  player.w00t
-  puts player
-end
-
-# Later on we'll make it more random. Here's an example of the output you're aiming for:
-=begin
-
-Moe got blammed!
-Moe got w00ted!
-Moe got w00ted!
-I'm Moe with a health of 120 and a score of 123.
-Larry got blammed!
-Larry got w00ted!
-Larry got w00ted!
-I'm Larry with a health of 80 and a score of 85.
-Curly got blammed!
-Curly got w00ted!
-Curly got w00ted!
-I'm Curly with a health of 145 and a score of 150.
 
 =end
