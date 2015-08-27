f3b37b6dc8f77e04b921fed88ed27646130ed4bf
kangkyu <kangkyu1111@gmail.com>
2015-08-27 10:33:41 -0700

    To learn more about arrays, iterators, and blocks refer to Section 4.1, Arrays and Section 4.3, Blocks and Iterators in Programming Ruby.

--- studio_game/studio_game.rb
+++ studio_game/studio_game.rb
@@ -30,23 +30,44 @@ class Player
   end
 end
 
-# In the end, we want to be able to run this code:
+# We currently have three Player objects that we created this way:
 
+player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
-puts player2.name
-player2.name = "Lawrence"
-puts player2.name
-puts player2.health
-puts player2.score
-puts player2
+player3 = Player.new("curly", 125)
 
-# and get this output:
+# Then, once we have our player objects in an array, we can iterate through them and do the same thing to each player. We'll start by iterating through them and printing out each player's information like this:
 =begin
 
-Larry
-Lawrence
-60
-68
-I'm Lawrence with a health of 60 and a score of 68.
+There are 3 players in the game:
+I'm Moe with a health of 100 and a score of 103.
+I'm Larry with a health of 60 and a score of 65.
+I'm Curly with a health of 125 and a score of 130.
+
+=end
+
+players = [player1, player2, player3]
+players.each do |player|
+  player.blam
+  player.w00t
+  player.w00t
+  puts player
+end
+
+# Later on we'll make it more random. Here's an example of the output you're aiming for:
+=begin
+
+Moe got blammed!
+Moe got w00ted!
+Moe got w00ted!
+I'm Moe with a health of 120 and a score of 123.
+Larry got blammed!
+Larry got w00ted!
+Larry got w00ted!
+I'm Larry with a health of 80 and a score of 85.
+Curly got blammed!
+Curly got w00ted!
+Curly got w00ted!
+I'm Curly with a health of 145 and a score of 150.
 
 =end
